3b09c89bdb356346d2ded4e376f576fe42c9174b
cosmonawt <cosmonawt@protonmail.ch>
2018-05-12 20:22:18 +0200

    Add Options call to CORS

--- main.go
+++ main.go
@@ -23,7 +23,13 @@ func main() {
 
 	router.Use(func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, PUT")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
 		c.Next()
 	})
 
